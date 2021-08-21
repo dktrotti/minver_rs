5c10a949473025a784a957495355aaea9d4df091
Dominic Trottier <domtrottier@outlook.com>
2021-08-20 21:40:28 -0700

    Implemented basic tag retrieval

--- src/git.rs
+++ src/git.rs
@@ -1,3 +1,8 @@
-pub fn get_tag() -> String {
-    return String::from("1.0.0")
-}
+// use git2;
+
+// pub fn get_tag(path: &std::path::Path, pattern: &str) -> Result<String, git2::Error> {
+//     let repo = git2::Repository::open_ext(path, git2::RepositoryOpenFlags::NO_SEARCH, std::iter::empty::<String>())?;
+//     let tags = repo.tag_names(Some(pattern))?;
+
+//     return String::from("1.0.0")
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,41 @@
-mod core;
-mod git;
+// mod core;
+// mod git;
+
+use git2;
 
 fn main() {
-    println!("{}", core::get_version());
+    match get_string(std::path::Path::new("./sampleRepo")) {
+        Ok(s) => println!("{}", s),
+        Err(e) => println!("Error: {}", e)
+    }
+}
+
+fn get_string(path: &std::path::Path) -> Result<String, git2::Error> {
+    let repo = git2::Repository::open_ext(path, git2::RepositoryOpenFlags::NO_SEARCH, std::iter::empty::<String>())?;
+    let tags = repo.tag_names(None)?;
+
+    let tagged_commits = tags.iter()
+        .filter_map(std::convert::identity)
+        .map(|tag_name| {
+            get_commit(&repo, tag_name)
+        })
+        .filter_map(|result| {
+            result.ok()
+        });
+
+    let tag_string = tagged_commits
+        .map(|commit| {
+            format!("{} = {}", commit.0, commit.1.id())
+        })
+        .reduce(|a, b| {
+            format!("{}, {}", a, b)
+        })
+        .unwrap_or(String::from(""));
+
+    Ok(tag_string)
+}
+
+fn get_commit<'a>(repository: &'a git2::Repository, tag_name: &'a str) -> Result<(&'a str, git2::Commit<'a>), git2::Error> {
+    let object = repository.revparse_single(&format!("refs/tags/{}", tag_name))?;
+    Ok((tag_name, object.peel_to_commit()?))
 }
