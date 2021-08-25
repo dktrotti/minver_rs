f5aeb87418b4809fd51f5fe7472bffabcdcedcf1
Dominic Trottier <domtrottier@outlook.com>
2021-08-24 21:39:23 -0700

    Refactored out test helper

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,45 +1,17 @@
-use git2::{Repository, Signature};
+use git2::Repository;
 use tempfile::TempDir;
 
 use minver_rs::*;
 
+mod repo_test_helper;
+
 #[test]
 fn test_tagged_head_returns_tag_version() {
     let dir = TempDir::new().unwrap();
-    let repo = Repository::init(dir.path()).unwrap();
+    let repo = repo_test_helper::create_temp_repo(dir.path()).unwrap();
 
-    let signature = Signature::now("testName", "test@example.com").unwrap();
-    let mut index = repo.index().unwrap();
-    let tree_id = index.write_tree().unwrap();
-    let tree = repo.find_tree(tree_id).unwrap();
-
-    let commit_id_1 = repo.commit(
-        Some("HEAD"),
-        &signature,
-        &signature,
-        "test message",
-        &tree,
-        &[]
-    ).unwrap();
-    let commit_1 = repo.find_commit(commit_id_1).unwrap();
-
-    let commit_id_2 = repo.commit(
-        Some("HEAD"),
-        &signature,
-        &signature,
-        "test message 2",
-        &tree,
-        &[&commit_1]
-    ).unwrap();
-    let commit_2 = repo.find_commit(commit_id_2).unwrap();
-
-    repo.tag(
-        "1.2.3",
-        &commit_2.as_object(),
-        &signature,
-        "created tag",
-        false
-    ).unwrap();
+    repo_test_helper::commit_on_head(&repo).unwrap();
+    repo_test_helper::tag_head(&repo, "1.2.3").unwrap();
 
     assert_eq!(
         Version { major: 1, minor: 2, patch: 3, prerelease: None, build_metadata: None },
---
+++ tests/repo_test_helper/mod.rs
@@ -0,0 +1,56 @@
+use git2::{Repository, Signature, Tree, Commit, Oid};
+use anyhow::Result;
+use std::path::Path;
+
+pub fn create_temp_repo(path: &Path) -> Result<Repository> {
+    let repo = Repository::init(path).unwrap();
+
+    {
+        let mut index = repo.index()?;
+        let tree_id = index.write_tree()?;
+        let tree = repo.find_tree(tree_id)?;
+
+        let signature = Signature::now("testName", "test@example.com")?;
+        repo.commit(
+            Some("HEAD"),
+            &signature,
+            &signature,
+            "message",
+            &tree,
+            &[]
+        )?;
+    }
+
+    Ok(repo)
+}
+
+pub fn commit_on_head(repo: &Repository) -> Result<Commit> {
+    let head_commit = get_head(repo)?;
+
+    let signature = Signature::now("testName", "test@example.com")?;
+    let commit_id = repo.commit(
+        Some("HEAD"),
+        &signature,
+        &signature,
+        "message",
+        &head_commit.tree()?,
+        &[&head_commit] 
+    )?;
+    Ok(repo.find_commit(commit_id)?)
+}
+
+pub fn tag_head(repo: &Repository, tag: &str) -> Result<Oid> {
+    let head_commit = get_head(repo)?;
+
+    let signature = Signature::now("testName", "test@example.com")?;
+    Ok(repo.tag(
+        tag,
+        head_commit.as_object(),
+        &signature,
+        "message",
+        false)?)
+}
+
+fn get_head(repo: &Repository) -> Result<Commit> {
+    Ok(repo.head()?.peel_to_commit()?)
+}
