e30b362018525eeebe5e5c913968277f823d8369
Dominic Trottier <domtrottier@outlook.com>
2021-08-28 15:38:40 -0700

    Removed unused files

--- src/core.rs
+++
@@ -1,3 +0,0 @@
-// pub fn get_version() -> String {
-//     crate::git::get_tag()
-// }
--- src/git.rs
+++
@@ -1,8 +0,0 @@
-// use git2;
-
-// pub fn get_tag(path: &std::path::Path, pattern: &str) -> Result<String, git2::Error> {
-//     let repo = git2::Repository::open_ext(path, git2::RepositoryOpenFlags::NO_SEARCH, std::iter::empty::<String>())?;
-//     let tags = repo.tag_names(Some(pattern))?;
-
-//     return String::from("1.0.0")
-// }
