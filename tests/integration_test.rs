5ca564cf5beeacc675a8a9b9f0e92c447340e77b
Dominic Trottier <domtrottier@outlook.com>
2021-08-27 20:49:14 -0700

    Implemented first branching test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -9,7 +9,7 @@ fn test_tagged_head_returns_tag_version() {
     let dir = TempDir::new().unwrap();
     let repo = repo_test_helper::create_temp_repo(dir.path()).unwrap();
 
-    repo_test_helper::commit_on_head(&repo).unwrap();
+    repo_test_helper::commit_on_head(&repo, "m").unwrap();
     repo_test_helper::tag_head(&repo, "1.2.3").unwrap();
 
     assert_eq!(
@@ -22,9 +22,9 @@ fn test_height_is_appended_to_version() {
     let dir = TempDir::new().unwrap();
     let repo = repo_test_helper::create_temp_repo(dir.path()).unwrap();
 
-    repo_test_helper::commit_on_head(&repo).unwrap();
+    repo_test_helper::commit_on_head(&repo, "m1").unwrap();
     repo_test_helper::tag_head(&repo, "1.2.3").unwrap();
-    repo_test_helper::commit_on_head(&repo).unwrap();
+    repo_test_helper::commit_on_head(&repo, "m2").unwrap();
 
     assert_eq!(
         Version { major: 1, minor: 2, patch: 3, prerelease: Some(String::from("alpha.1")), build_metadata: None },
@@ -36,8 +36,8 @@ fn test_when_no_tags_are_present_in_ancestors_then_default_version_is_returned()
     let dir = TempDir::new().unwrap();
     let repo = repo_test_helper::create_temp_repo(dir.path()).unwrap();
 
-    repo_test_helper::commit_on_head(&repo).unwrap();
-    repo_test_helper::commit_on_head(&repo).unwrap();
+    repo_test_helper::commit_on_head(&repo, "m1").unwrap();
+    repo_test_helper::commit_on_head(&repo, "m2").unwrap();
 
     assert_eq!(
         Version { major: 0, minor: 0, patch: 0, prerelease: Some(String::from("alpha.0")), build_metadata: None },
@@ -49,8 +49,8 @@ fn test_when_repo_access_fails_then_error_is_returned() {
     let dir = TempDir::new().unwrap();
     let repo = repo_test_helper::create_temp_repo(dir.path()).unwrap();
 
-    repo_test_helper::commit_on_head(&repo).unwrap();
-    repo_test_helper::commit_on_head(&repo).unwrap();
+    repo_test_helper::commit_on_head(&repo, "m1").unwrap();
+    repo_test_helper::commit_on_head(&repo, "m2").unwrap();
 
     dir.close().unwrap();
 
@@ -63,9 +63,9 @@ fn test_when_lower_tag_is_more_recent_then_older_version_is_returned() {
     let dir = TempDir::new().unwrap();
     let repo = repo_test_helper::create_temp_repo(dir.path()).unwrap();
 
-    repo_test_helper::commit_on_head(&repo).unwrap();
+    repo_test_helper::commit_on_head(&repo, "m1").unwrap();
     repo_test_helper::tag_head(&repo, "2.0.0").unwrap();
-    repo_test_helper::commit_on_head(&repo).unwrap();
+    repo_test_helper::commit_on_head(&repo, "m2").unwrap();
     repo_test_helper::tag_head(&repo, "1.2.3").unwrap();
 
     assert_eq!(
@@ -78,10 +78,10 @@ fn test_when_old_commit_is_checked_out_then_newer_tags_are_ignored() {
     let dir = TempDir::new().unwrap();
     let repo = repo_test_helper::create_temp_repo(dir.path()).unwrap();
 
-    repo_test_helper::commit_on_head(&repo).unwrap();
+    repo_test_helper::commit_on_head(&repo, "m1").unwrap();
     repo_test_helper::tag_head(&repo, "1.2.3").unwrap();
-    let intermediate_commit = repo_test_helper::commit_on_head(&repo).unwrap();
-    repo_test_helper::commit_on_head(&repo).unwrap();
+    let intermediate_commit = repo_test_helper::commit_on_head(&repo, "m2").unwrap();
+    repo_test_helper::commit_on_head(&repo, "m3").unwrap();
     repo_test_helper::tag_head(&repo, "1.3.0").unwrap();
 
     repo_test_helper::checkout_commit(&repo, &intermediate_commit).unwrap();
@@ -92,14 +92,30 @@ fn test_when_old_commit_is_checked_out_then_newer_tags_are_ignored() {
 }
 
 #[test]
-#[ignore]
-fn test_when_branches_diverge_then_higher_tag_is_used() {
-    assert!(true, "Not implemented")
+fn test_when_branches_diverge_with_multiple_tags_then_higher_tag_is_used() {
+    let dir = TempDir::new().unwrap();
+    let repo = repo_test_helper::create_temp_repo(dir.path()).unwrap();
+
+    let commit_1 = repo_test_helper::commit_on_head(&repo, "c1").unwrap();
+
+    let branch_1_commit_1 = repo_test_helper::commit_with_parent(&repo, &commit_1, "b1c1").unwrap();
+    repo_test_helper::tag_commit(&repo, &branch_1_commit_1, "1.3.0").unwrap();
+    let branch_1_commit_2 = repo_test_helper::commit_with_parent(&repo, &branch_1_commit_1, "b1c2").unwrap();
+
+    repo_test_helper::checkout_commit(&repo, &commit_1).unwrap();
+    let branch_2_commit_1 = repo_test_helper::commit_with_parent(&repo, &commit_1, "b2c1").unwrap();
+    repo_test_helper::tag_commit(&repo, &branch_2_commit_1, "1.2.3").unwrap();
+
+    repo_test_helper::merge_commit(&repo, &[&branch_2_commit_1, &branch_1_commit_2], "m").unwrap();
+
+    assert_eq!(
+        Version { major: 1, minor: 3, patch: 0, prerelease: Some(String::from("alpha.2")), build_metadata: None },
+        minver_rs::get_version(&repo).unwrap());
 }
 
 #[test]
 #[ignore]
-fn test_when_branches_merge_then_lower_height_is_used() {
+fn test_when_branches_merge_with_same_tagged_parent_then_lower_height_is_used() {
     assert!(true, "Not implemented")
 }
 
--- tests/repo_test_helper/mod.rs
+++ tests/repo_test_helper/mod.rs
@@ -24,28 +24,38 @@ pub fn create_temp_repo(path: &Path) -> Result<Repository> {
     Ok(repo)
 }
 
-pub fn commit_on_head(repo: &Repository) -> Result<Commit> {
+pub fn commit_on_head<'a>(repo: &'a Repository, message: &str) -> Result<Commit<'a>> {
     let head_commit = get_head(repo)?;
+    commit_with_parent(repo, &head_commit, message)
+}
 
+pub fn commit_with_parent<'a>(repo: &'a Repository, commit: &Commit, message: &str) -> Result<Commit<'a>> {
+    merge_commit(repo, &[&commit], message)
+}
+
+pub fn merge_commit<'a>(repo: &'a Repository, parents: &[&Commit], message: &str) -> Result<Commit<'a>> {
     let signature = Signature::now("testName", "test@example.com")?;
     let commit_id = repo.commit(
         Some("HEAD"),
         &signature,
         &signature,
-        "message",
-        &head_commit.tree()?,
-        &[&head_commit] 
+        message,
+        &parents[0].tree()?,
+        &parents 
     )?;
     Ok(repo.find_commit(commit_id)?)
 }
 
 pub fn tag_head(repo: &Repository, tag: &str) -> Result<Oid> {
     let head_commit = get_head(repo)?;
+    tag_commit(repo, &head_commit, tag)
+}
 
+pub fn tag_commit(repo: &Repository, commit: &Commit, tag: &str) -> Result<Oid> {
     let signature = Signature::now("testName", "test@example.com")?;
     Ok(repo.tag(
         tag,
-        head_commit.as_object(),
+        commit.as_object(),
         &signature,
         "message",
         false)?)
