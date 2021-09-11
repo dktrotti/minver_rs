2d88419b663ff1a2040fdc1d9cb60e74a0bd9e57
Dominic Trottier <domtrottier@outlook.com>
2021-09-11 16:42:03 -0700

    Updated build script to use CARGO_MANIFEST_DIR

--- build.rs
+++ build.rs
@@ -2,10 +2,11 @@ use anyhow::Result;
 use git2::Repository;
 use minver_rs;
 use std::env;
+use std::ffi::OsString;
 use std::fs;
+use std::path::{Path, PathBuf};
 use toml_edit::{value, Document};
 
-const MANIFEST_PATH: &str = "./Cargo.toml";
 const UPDATE_VERSION_VAR: &str = "MINVER_UPDATE_VERSION";
 
 fn main() {
@@ -19,7 +20,8 @@ fn main() {
 }
 
 fn set_package_version() -> Result<()> {
-    let mut document: Document = fs::read_to_string(MANIFEST_PATH)?.parse::<Document>()?;
+    let manifest_path = get_manifest_path();
+    let mut document: Document = fs::read_to_string(&manifest_path)?.parse::<Document>()?;
 
     let repo = Repository::open(".")?;
     let version = minver_rs::get_version(&repo)?;
@@ -27,7 +29,12 @@ fn set_package_version() -> Result<()> {
     document["package"]["version"] = value(version.to_string());
 
     Ok(fs::write(
-        MANIFEST_PATH,
+        &manifest_path,
         document.to_string_in_original_order(),
     )?)
+}
+
+fn get_manifest_path() -> PathBuf {
+    let manifest_dir = &env::var_os("CARGO_MANIFEST_DIR").unwrap_or(OsString::from("."));
+    Path::new(manifest_dir).join("Cargo.toml")
 }
