bea3950a5d9c71a49bf72e1edecf00b55c540157
Dominic Trottier <domtrottier@outlook.com>
2021-09-11 17:33:13 -0700

    Fixed bug in previous commit

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use minver_rs;
 use std::env;
 use std::ffi::OsString;
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use toml_edit::{value, Document};
 
 const UPDATE_VERSION_VAR: &str = "MINVER_UPDATE_VERSION";
@@ -20,10 +20,12 @@ fn main() {
 }
 
 fn set_package_version() -> Result<()> {
-    let manifest_path = get_manifest_path();
+    let manifest_dir = &env::var_os("CARGO_MANIFEST_DIR").unwrap_or(OsString::from("."));
+    let manifest_path = Path::new(manifest_dir).join("Cargo.toml");
+
     let mut document: Document = fs::read_to_string(&manifest_path)?.parse::<Document>()?;
 
-    let repo = Repository::open(&manifest_path)?;
+    let repo = Repository::open(manifest_dir)?;
     let version = minver_rs::get_version(&repo)?;
 
     document["package"]["version"] = value(version.to_string());
@@ -32,9 +34,4 @@ fn set_package_version() -> Result<()> {
         &manifest_path,
         document.to_string_in_original_order(),
     )?)
-}
-
-fn get_manifest_path() -> PathBuf {
-    let manifest_dir = &env::var_os("CARGO_MANIFEST_DIR").unwrap_or(OsString::from("."));
-    Path::new(manifest_dir).join("Cargo.toml")
 }
