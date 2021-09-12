f38d42a60821f20a3d89870af6e98ca87710aab0
Dominic Trottier <domtrottier@outlook.com>
2021-09-11 18:18:50 -0700

    Made build_utils module public

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
-mod build_utils;
+pub mod build_utils;
 mod minver_core;
 mod semver;
 
 pub use minver_core::get_version;
 pub use minver_core::Version;
-
-pub use build_utils::default_build_action;
-pub use build_utils::update_package_version;
-pub use build_utils::UPDATE_VERSION_VAR;
